768c11504d9220eb972bc61e147732c33ef524d5
williamstyronejr <itenory@gmail.com>
2023-02-22 00:52:31 -0500

    Initial bootstrap

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Habit Tracker</title>
+      </Head>
+
+      <main className="">placeholder</main>
+    </>
+  );
+}
