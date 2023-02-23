e685e481aaa765cd28ae86c7bb2246bf534b8897
williamstyronejr <itenory@gmail.com>
2023-02-23 14:23:16 -0500

    Add functionality for  completing habit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,76 @@
 import Head from 'next/head';
+import { useCallback, useEffect, useState } from 'react';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+import { Habit } from '@prisma/client';
+
+function isSameDay(date1: Date | null, date2: Date | null) {
+  return date1 === null || date2 === null
+    ? false
+    : date1.getUTCFullYear() === date2.getUTCFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate();
+}
 
 export default function Home() {
+  const [habits, setHabits] = useState<Habit[]>([]);
+  const [parent, enableAnimations] = useAutoAnimate();
+
+  useEffect(() => {
+    fetch('/api/habits').then(async (res) => {
+      if (!res.ok) return;
+      const body: { habits: Habit[] } = await res.json();
+      setHabits(body.habits);
+    });
+  }, []);
+
+  const updateHabit = useCallback((id: String) => {
+    fetch(`/api/habits/complete?id=${id}`, { method: 'POST' }).then(() => {});
+  }, []);
+
+  console.log(habits);
+
   return (
     <>
       <Head>
         <title>Habit Tracker</title>
       </Head>
 
-      <main className="">placeholder</main>
+      <section>
+        <header></header>
+
+        <div>
+          <ul ref={parent}>
+            {habits
+              .filter(
+                (habit) =>
+                  !isSameDay(new Date(), new Date(habit.lastCompleted || ''))
+              )
+              .map((habit) => (
+                <li key={habit.id} className="">
+                  <button
+                    className=""
+                    type="button"
+                    onClick={() => {
+                      setHabits((old) =>
+                        old.map((item) => {
+                          return item.id !== habit.id
+                            ? item
+                            : {
+                                ...item,
+                                lastCompleted: new Date().toDateString(),
+                              };
+                        })
+                      );
+                      updateHabit(habit.id);
+                    }}
+                  >
+                    {habit.title}
+                  </button>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
