1949ed893be805155c4edbeedf2583a0e21cd60e
williamstyronejr <itenory@gmail.com>
2023-03-31 00:54:43 -0400

    Simply way to track streak

--- src/pages/api/habits/index.ts
+++ src/pages/api/habits/index.ts
@@ -1,20 +1,59 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '@/utils/db';
-import { Habit, Completion } from '@prisma/client';
+import { Habit, Completion, Icon } from '@prisma/client';
 import dayjs from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+dayjs.extend(isSameOrAfter);
 
 type ResponseData = {
   habits: (Habit & {
     Completion: Completion[];
+    icon: Icon;
   })[];
 };
+
+async function updateHabits(
+  habits: (Habit & {
+    Completion: Completion[];
+    icon: Icon;
+  })[]
+) {
+  const dayBeforeCurrent = dayjs().startOf('day').subtract(1, 'day');
+
+  const habitToUpdate: number[] = [];
+
+  habits.forEach((habit) => {
+    const lastDate = dayjs(habit.lastCompleted).startOf('day');
+    if (!lastDate.isSameOrAfter(dayBeforeCurrent)) {
+      habitToUpdate.push(habit.id);
+    }
+  });
+
+  if (habitToUpdate.length) {
+    await prisma.habit.updateMany({
+      where: {
+        id: {
+          in: habitToUpdate,
+        },
+      },
+      data: {
+        completionStreak: 0,
+      },
+    });
+  }
+
+  return habitToUpdate.length > 0;
+}
 
 export default async function RequestHandler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData | String>
 ) {
   try {
-    const habits = await prisma.habit.findMany({
+    let habits = await prisma.habit.findMany({
+      orderBy: {
+        title: 'asc',
+      },
       include: {
         icon: {},
         Completion: {
@@ -26,6 +65,26 @@ export default async function RequestHandler(
         },
       },
     });
+
+    const needsUpdate = await updateHabits(habits);
+
+    if (needsUpdate) {
+      habits = await prisma.habit.findMany({
+        orderBy: {
+          title: 'asc',
+        },
+        include: {
+          icon: {},
+          Completion: {
+            where: {
+              date: {
+                gte: dayjs().startOf('w').toDate(),
+              },
+            },
+          },
+        },
+      });
+    }
 
     return res.json({ habits });
   } catch (err) {
